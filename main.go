f7b01c5d184149818b22b01b70a310ba585c75de
Matthew Gleich <email@mattglei.ch>
2020-11-19 13:41:29 -0500

    ✨ Add basic arg parsing and level calculations
    Signed-off-by: Matthew Gleich <email@mattglei.ch>

--- main.go
+++ main.go
@@ -1,9 +1,42 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"time"
+
 	"github.com/Matt-Gleich/logoru"
 )
 
+var (
+	timeLevel  string
+	lightLevel int
+)
+
 func main() {
-	logoru.Success("Program booted up!")
+	parseArgs()
+	getLevel()
+	fmt.Println(lightLevel)
+}
+
+func parseArgs() {
+	timeLevel = os.Args[0]
+	logoru.Info("Got time level")
+}
+
+// Get the light level
+func getLevel() {
+	var (
+		now        = time.Now()
+		percentage float32
+	)
+	switch timeLevel {
+	case "hours":
+		percentage = float32(now.Hour()) / 24
+	case "minutes":
+		percentage = float32(now.Minute()) / 60
+	default:
+		percentage = float32(now.Second()) / 60
+	}
+	lightLevel = int(percentage * 10)
 }
